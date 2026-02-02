ddda27aa441d452a382c9d9d6ed18a013e52eacf
Keeso1 <isac.lindh01@gmail.com>
2026-02-02 16:15:31 +0100

    feat: Add markdown support

--- components/ai-component.tsx
+++ components/ai-component.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { generateArticle } from "@/lib/ai";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export default function AiComponent() {
   const [prompt, setPrompt] = useState("");
@@ -12,7 +14,13 @@ export default function AiComponent() {
 
   return (
     <>
-      {isLoading ? "Loading.." : article}
+      {isLoading ? (
+        "Loading.."
+      ) : (
+        <div className="prose">
+          <Markdown remarkPlugins={[remarkGfm]}>{article}</Markdown>
+        </div>
+      )}
       <Input
         onChange={(e) => {
           setPrompt(e.target.value);
