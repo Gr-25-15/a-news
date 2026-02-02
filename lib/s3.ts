b3cbf74fece71637ee37139f540473e232261c21
Samuel Ward <samuelleeward9@gmail.com>
2026-02-02 10:38:14 +0100

    feat: implement S3 client and upload URL generation function

---
+++ lib/s3.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3Client = new S3Client({
+  region: process.env.S3_REGION || "",
+  endpoint: process.env.S3_ENDPOINT,
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+  },
+  forcePathStyle: true,
+});
+
+export async function getUploadUrl(filename: string, contentType: string) {
+  const key = `articles/${Date.now()}-${filename}`;
+
+  const command = new PutObjectCommand({
+    Bucket: process.env.S3_BUCKET!,
+    Key: key,
+    ContentType: contentType,
+  });
+
+  const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
+  const fileUrl = `${process.env.S3_ENDPOINT}/${process.env.S3_BUCKET}/${key}`;
+
+  return { uploadUrl: signedUrl, fileUrl, key };
+}
