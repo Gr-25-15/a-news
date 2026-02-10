f917e483f267d061343a22a6057e1e3b922db592
Justine-Wolf <justine.wolf@outlook.com>
2026-02-10 15:25:46 +0100

    betterauth roles and prisma article action
    Addition of roles to betterauth: admin, editor and user:
    Update of auth.ts and auth-client.ts with the roles.
    Creation of manage-article.ts files to add create, edit and delete prisma action.
    Creation permission.ts that define the different roles.

---
+++ app/actions/manage-article.ts
@@ -0,0 +1,127 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { headers } from "next/headers";
+
+export async function getAllArtcles() {
+    return await prisma.article.findMany();
+}
+
+export async function createArticle(
+  id: string,
+  contentUrl: string,
+  authorId: string,
+  categoryId: string,
+  userId: string,
+  title: string,
+  imageKey?: string,
+  featuredImage?: string,
+  editorId?: string,
+  type?: string,
+) {
+  const { success } = await auth.api.userHasPermission({
+    headers: await headers(),
+    body: {
+      permissions: {
+        article: ["create"],
+      },
+    },
+  });
+  if (!success) {
+    return { success: false, error: "Unauthorized" };
+  }
+  try {
+    await prisma.article.create({
+      data: {
+        id,
+        contentUrl,
+        authorId,
+        categoryId,
+        userId,
+        title,
+        imageKey,
+        featuredImage,
+        editorId,
+        type,
+      },
+    });
+    revalidatePath("/");
+    return { success: true };
+  } catch (err) {
+    return { success: false, error: "Failed to create article" };
+  }
+}
+
+export async function editArticle(
+  id: string,
+  contentUrl: string,
+  authorId: string,
+  categoryId: string,
+  userId: string,
+  title: string,
+  imageKey?: string,
+  featuredImage?: string,
+  editorId?: string,
+  type?: string,
+) {
+  const { success } = await auth.api.userHasPermission({
+    headers: await headers(),
+    body: {
+      permissions: {
+        article: ["edit"],
+      },
+    },
+  });
+  if (!success) {
+    return { success: false, error: "Unauthorized" };
+  }
+  try {
+    await prisma.article.update({
+      where: {
+        id,
+      },
+      data: {
+        contentUrl,
+        authorId,
+        categoryId,
+        userId,
+        title,
+        imageKey,
+        featuredImage,
+        editorId,
+        type,
+      },
+    });
+    revalidatePath("/");
+    return { success: true };
+  } catch (err) {
+    return { success: false, error: "Failed to edit article" };
+  }
+}
+
+export async function deleteArticle(id: string) {
+  const { success } = await auth.api.userHasPermission({
+    headers: await headers(),
+    body: {
+      permissions: {
+        article: ["delete"],
+      },
+    },
+  });
+  if (!success) {
+    return { success: false, error: "Unauthorized" };
+  }
+  try {
+    await prisma.article.delete({
+      where: {
+        id,
+      },
+    });
+    revalidatePath("/");
+    return { success: true };
+  } catch (err) {
+    return { success: false, error: "Failed to delete article" };
+  }
+}
