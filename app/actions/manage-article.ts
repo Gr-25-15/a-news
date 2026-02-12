1c0759bf48cfbf5e8da47f27b5ef43a345549900
Keeso1 <isac.lindh01@gmail.com>
2026-02-12 15:03:37 +0100

    fix: updated createarticle and added user ownership of articles on creation

--- app/actions/manage-article.ts
+++ app/actions/manage-article.ts
@@ -4,51 +4,86 @@ import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
+import { s3Client, S3_BUCKET } from "@/lib/s3";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 
 export async function getAllArticles() {
   return await prisma.article.findMany();
 }
 
 export async function createArticle(
-  id: string,
-  contentUrl: string,
-  categoryId: string,
   title: string,
-  imageKey?: string,
-  featuredImage?: string,
-  type?: string,
+  categoryId: string,
+  isPublished: boolean,
+  isSubscriberOnly: boolean,
+  thumbnailUrl?: string,
+  description?: string,
+  content?: string,
 ) {
-  const { success } = await auth.api.userHasPermission({
-    headers: await headers(),
-    body: {
-      permissions: {
-        article: ["create"],
-      },
-    },
-  });
-  if (!success) {
-    return { success: false, error: "Unauthorized" };
-  }
-
-  const authorId = (await auth.api.getUser()).id;
+  const session = await auth.api.getSession();
 
   try {
-    await prisma.article.create({
-      data: {
-        id,
-        contentUrl,
-        authorId,
-        categoryId,
-        title,
+    // Check permissions
+    const { success } = await auth.api.userHasPermission({
+      headers: await headers(),
+      body: {
+        permissions: {
+          article: ["create"],
+        },
       },
     });
+    if (!success) {
+      throw new Error("unauthorized");
+    }
+
+    if (!session) {
+      throw new Error("No session");
+    }
+
+    const userId = session.user.id;
+    const slug = title
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/(^-|-$)/g, "");
+
+    const filename = `${slug}-${Date.now()}.md`;
+    const s3Key = `articles/${filename}`;
+
+    // Upload to S3
+    console.log(`Uploading content to S3: ${s3Key}`);
+    await s3Client.send(
+      new PutObjectCommand({
+        Bucket: S3_BUCKET,
+        Key: s3Key,
+        Body: content,
+        ContentType: "text/markdown",
+      }),
+    );
+
+    const contentUrl = `${process.env.S3_ENDPOINT}/${S3_BUCKET}/${s3Key}`;
+
+    // Create Article in DB
+    const article = await prisma.article.create({
+      data: {
+        title,
+        contentUrl: contentUrl,
+        categoryId,
+        authorId: userId,
+        description,
+        isPublished,
+        isSubscriberOnly,
+        thumbnailUrl,
+      },
+    });
+
     revalidatePath("/");
-    return { success: true };
-  } catch (err) {
-    return { success: false, error: "Failed to create article" };
+    console.log(`Successfully created article: ${title}`);
+    return { success: true, article: article };
+  } catch (e) {
+    console.error(`Error processing topic "${title}":`, e);
+    return { success: false, error: e };
   }
 }
-
 export async function editArticle(
   id: string,
   contentUrl: string,
--- components/create-article-form.tsx
+++ components/create-article-form.tsx
@@ -7,26 +7,76 @@ import {
 import { useRef, useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { Label } from "./ui/label";
-import { createArticle } from "@/app/actions/createArticle";
+import { useSession } from "@/lib/auth-client";
+import { createArticle } from "@/app/actions/manage-article";
+import z from "zod";
+import { useForm, FormProvider } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Field, FieldContent, FieldError, FieldLabel } from "./ui/field";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Switch } from "./ui/switch";
+import { Option } from "@/app/actions/getCategories";
 
-export default function CreateArticle() {
-  const [title, setTitle] = useState("");
+const schema = z.object({
+  title: z.string().min(1, "Title is required"),
+  description: z.string().min(1, "Description is required"),
+  categoryId: z.string().min(1),
+  isPublished: z.boolean(),
+  isSubscriberOnly: z.boolean(),
+  thumbnailUrl: z.string().min(1),
+});
+
+export type createArticleFormData = z.infer<typeof schema>;
+
+export default function CreateArticle(
+  session: ReturnType<typeof useSession>,
+  categories: Option[],
+) {
   const editorRef = useRef<ShadcnTemplateRef>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
+  const form = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      title: "",
+      description: "",
+      categoryId: "",
+      isPublished: false,
+      isSubscriberOnly: false,
+      thumbnailUrl: "",
+    },
+  });
   const handleSave = () => {
+    setIsLoading(true);
     const markdownContent = editorRef.current?.getMarkdown();
     console.log("Title:", title);
     console.log("Markdown Content:", markdownContent);
     // We will call the server action here
-    createArticle(
-      title,
-      "seed-user-id",
-      "seed-user-id",
-      "sweden",
-      "news",
-      markdownContent,
-    );
+    try {
+      if (!session.data) {
+        throw new Error("Session not found");
+      }
+      createArticle(
+        title,
+        "categoryId(fix)",
+        isPublished,
+        isSubscriberOnly,
+        "thumbnail-image-url",
+        description,
+        markdownContent,
+      );
+    } catch (error) {
+      console.error((error as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -35,19 +85,60 @@ export default function CreateArticle() {
         <CardTitle>Create Article</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-2 mb-4">
-          <Label htmlFor="title">Title</Label>
-          <Input
-            id="title"
-            placeholder="Article Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
+        <FormProvider {...form}>
+          <form onSubmit={form.handleSubmit(handleSave)}>
+            <Field>
+              <FieldLabel>Title</FieldLabel>
+              <FieldContent>
+                <Input {...form.register("title")} disabled={isLoading} />
+                <FieldError errors={[form.formState.errors.title]} />
+              </FieldContent>
+            </Field>
+
+            <Field>
+              <FieldLabel>Description</FieldLabel>
+              <FieldContent>
+                <Input {...form.register("description")} disabled={isLoading} />
+                <FieldError errors={[form.formState.errors.description]} />
+              </FieldContent>
+            </Field>
+
+            <Field>
+              <FieldLabel>Category</FieldLabel>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Choose category" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {categories.map((category) => (
+                      <SelectItem key={category.value} value={category.value}>
+                        {category.label}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </Field>
+
+            <Field orientation="horizontal" className="w-fit">
+              <FieldLabel htmlFor="isPublished">Publish on save:</FieldLabel>
+              <Switch id="isPublished" />
+            </Field>
+
+            <Field orientation="horizontal" className="w-fit">
+              <FieldLabel htmlFor="isSubscriberOnly">
+                Subscriber only:
+              </FieldLabel>
+              <Switch id="isPublished" />
+            </Field>
+
+            <Button type="submit" disabled={isLoading}>
+              Create Movie
+            </Button>
+          </form>
+        </FormProvider>
         <ShadcnTemplate ref={editorRef} />
-        <Button onClick={handleSave} className="mt-4">
-          Save
-        </Button>
       </CardContent>
     </Card>
   );
